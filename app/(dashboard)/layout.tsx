76a76a490161ad5713c40a97b5032ceb0c194b96
DungLe2983 <lequocdung2983@gmail.com>
2024-05-21 23:34:18 +0700

    fix: change title,description for SEO

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,7 +9,15 @@ import '../globals.css';
 import LeftSideBar from '@/components/layout/LeftSideBar';
 import TopBar from '@/components/layout/TopBar';
 import { ToasterProvider } from '@/lib/ToasterProvider';
+import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
 
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'Admin Dashboard',
+    description: "Admin dashboard to manage FashionFusion's data",
+};
 export default function RootLayout({
     children,
 }: {
@@ -19,10 +27,10 @@ export default function RootLayout({
         <ClerkProvider>
             <html lang='en'>
                 <body>
-                    <ToasterProvider/>
+                    <ToasterProvider />
                     <div className='flex max-lg:flex-col'>
                         <LeftSideBar />
-                        <TopBar/>
+                        <TopBar />
                         <main className='flex-1'>{children}</main>
                     </div>
                 </body>
