a9549b865a22be2aafea67ed248197c856d6cf7a
DungLe2983 <lequocdung2983@gmail.com>
2025-01-08 12:55:03 +0700

    update Promotion Column

--- components/promotions/PromotionColmns.tsx
+++ components/promotions/PromotionColmns.tsx
@@ -1,39 +1,44 @@
-'use client';
+"use client";
 
-import { ColumnDef } from '@tanstack/react-table';
-import Delete from '../custom-ui/Delete';
-import Link from 'next/link';
+import { ColumnDef } from "@tanstack/react-table";
+import Delete from "../custom-ui/Delete";
+import Link from "next/link";
 
 export const columns: ColumnDef<PromotionType>[] = [
-    {
-        accessorKey: 'code',
-        header: 'Code',
-        cell: ({ row }) => (
-            <Link
-                href={`/promotions/${row.original._id}`}
-                className='hover:text-blue-1'
-            >
-                {row.original.code}
-            </Link>
-        ),
-    },
-    {
-        accessorKey: 'name',
-        header: 'Name',
-        cell: ({ row }) => <p>{row.original.name}</p>,
-    },
-    {
-        accessorKey: 'percent',
-        header: 'Percent',
-        cell: ({ row }) => <p>{row.original.percent}%</p>,
-    },
-    {
-        accessorKey: 'price_promotion',
-        header: 'Price Promotions',
-        cell: ({ row }) => <p>{row.original.price_promotion.toLocaleString()}đ</p>,
-    },
-    {
-        accessorKey: 'Actions',
-        cell: ({ row }) => <Delete item='promotions' id={row.original._id} />,
-    },
+  {
+    accessorKey: "code",
+    header: "Code",
+    cell: ({ row }) => (
+      <Link
+        href={`/promotions/${row.original._id}`}
+        className='hover:text-blue-1'
+      >
+        {row.original.code}
+      </Link>
+    ),
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => <p>{row.original.name}</p>,
+  },
+  {
+    accessorKey: "percent",
+    header: "Percent",
+    cell: ({ row }) => <p>{row.original.percent}%</p>,
+  },
+  {
+    accessorKey: "price_promotion",
+    header: "Price Promotions",
+    cell: ({ row }) => <p>{row.original.price_promotion.toLocaleString()}đ</p>,
+  },
+  {
+    accessorKey: "count",
+    header: "Count",
+    cell: ({ row }) => <p>{row.original.count}</p>,
+  },
+  {
+    accessorKey: "Actions",
+    cell: ({ row }) => <Delete item='promotions' id={row.original._id} />,
+  },
 ];
